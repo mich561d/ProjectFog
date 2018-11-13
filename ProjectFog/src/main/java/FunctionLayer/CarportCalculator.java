790405c96c6d48f0e5fb1a24a1c8a4a1cc485120
Michael <mich561d@hotmail.com>
2018-11-13 13:06:27 +0100

    Added CarportCalculator and some tests

--- ProjectFog/src/main/java/FunctionLayer/CarportCalculator.java
+++ ProjectFog/src/main/java/FunctionLayer/CarportCalculator.java
@@ -1,14 +1,155 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FunctionLayer;
+
+import java.util.ArrayList;
 
 /**
  *
- * @author ryger
+ * @author Christian & Michael
  */
 public class CarportCalculator {
-    
+
+    // Data
+    private final int HEIGHT, LENGTH, WIDTH;
+    private ArrayList<String> products;
+
+    // Constructor
+    public CarportCalculator(int HEIGHT, int LENGTH, int WIDTH) {
+        this.HEIGHT = HEIGHT;
+        this.LENGTH = LENGTH;
+        this.WIDTH = WIDTH;
+        this.products = new ArrayList();
+    }
+
+    // Calc price
+    private double CalcTotalPrice() {
+        double totalPrice = 0.0;
+        for (int i = 0; i < products.size(); i++) {
+            // TODO: totalPrice += products.get(i).getPrice; ect...
+        }
+        return totalPrice;
+    }
+
+    public ArrayList<String> getProductList() {
+        return products;
+    }
+
+    // Calc  method:
+    public double CalcCarport() {
+        // Calc hjørne stolper
+        CalcPoles();
+        // Calc tag spær
+        CalcRoofRafter();
+        // Calc beklædning
+        CalcCladding();
+        // Calc tag
+        CalcRoof();
+
+        return CalcTotalPrice();
+    }
+
+    private void CalcPoles() {
+        int calcHeight = HEIGHT + 90; // height to be calc'ed
+        int poles = 0;
+        poles += CalcPolesSides();
+        poles += CalcPolesBack();
+
+        for (int i = 0; i < poles; i++) {
+            products.add("Pole, length: " + calcHeight);
+        }
+    }
+
+    public int CalcPolesSides() {
+        int poles = 0;
+        int maxDistance = 300;
+        double estPolesSides = ((float) LENGTH - 30) / (float) maxDistance;
+        poles += Math.ceil(estPolesSides) + 1;
+        poles *= 2; // For both sides
+        return poles;
+    }
+
+    public int CalcPolesBack() {
+        int poles = 0;
+
+        if (WIDTH >= 300) {
+            int maxDistance = 300;
+            double estPolesSides = ((float) WIDTH - 30) / (float) maxDistance;
+            poles += Math.ceil(estPolesSides) - 1;
+        }
+        return poles;
+    }
+
+    private void CalcRoofRafter() {
+        CalcRoofRafterWidth();
+        CalcRoofRafterLength();
+    }
+
+    public void CalcRoofRafterWidth() {
+        int calcWidth = WIDTH;
+        for (int width = calcWidth; width > 0;) {
+            int lengthOfRaft;
+            if (width >= 720) {
+                lengthOfRaft = 720;
+            } else if (width >= 660) {
+                lengthOfRaft = 660;
+            } else if (width >= 600) {
+                lengthOfRaft = 600;
+            } else if (width >= 540) {
+                lengthOfRaft = 540;
+            } else if (width >= 480) {
+                lengthOfRaft = 480;
+            } else if (width >= 420) {
+                lengthOfRaft = 420;
+            } else if (width >= 360) {
+                lengthOfRaft = 360;
+            } else if (width >= 300) {
+                lengthOfRaft = 300;
+            } else {
+                // What to do:
+                lengthOfRaft = 300;
+                // or
+                // lengthOfRaft = width;
+            }
+            products.add("Raft, length: " + lengthOfRaft);
+            width -= lengthOfRaft;
+        }
+    }
+
+    public void CalcRoofRafterLength() {
+        int calcLength = LENGTH;
+        for (int length = calcLength; length > 0;) {
+            int lengthOfRaft;
+            if (length >= 720) {
+                lengthOfRaft = 720;
+            } else if (length >= 660) {
+                lengthOfRaft = 660;
+            } else if (length >= 600) {
+                lengthOfRaft = 600;
+            } else if (length >= 540) {
+                lengthOfRaft = 540;
+            } else if (length >= 480) {
+                lengthOfRaft = 480;
+            } else if (length >= 420) {
+                lengthOfRaft = 420;
+            } else if (length >= 360) {
+                lengthOfRaft = 360;
+            } else if (length >= 300) {
+                lengthOfRaft = 300;
+            } else {
+                // What to do:
+                lengthOfRaft = 300;
+                // or
+                // lengthOfRaft = width;
+            }
+            products.add("Raft, length: " + lengthOfRaft);
+            length -= lengthOfRaft;
+        }
+    }
+
+    private void CalcCladding() {
+
+    }
+
+    private void CalcRoof() {
+
+    }
 }
---
+++ ProjectFog/src/test/java/FunctionLayer/CarportCalculatorTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionLayer;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Michael
+ */
+public class CarportCalculatorTest {
+
+    public CarportCalculatorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void CalcCarportLength() { // take length minus 30 so there is space for the roof
+        CarportCalculator CC = new CarportCalculator(210, 310, 270);
+        assertEquals(4, CC.CalcPolesSides()); // 310-30=280 : 2 for each side
+
+        CC = new CarportCalculator(210, 640, 270);
+        assertEquals(8, CC.CalcPolesSides()); // 640-30=610 : 4 for each side
+
+        CC = new CarportCalculator(210, 330, 270); 
+        assertEquals(4, CC.CalcPolesSides()); // 330-30=300 : 2 for each side
+
+        CC = new CarportCalculator(210, 350, 270);
+        assertEquals(6, CC.CalcPolesSides()); // 350-30=320 : 3 for each side
+
+        CC = new CarportCalculator(210, 10000, 270);
+        assertEquals(70, CC.CalcPolesSides()); // 10000-30=9970 : 35 for each side
+    }
+
+    @Test
+    public void CalcCarportWidth() { // take width minus 30 so there is space for the roof
+        CarportCalculator CC = new CarportCalculator(210, 310, 270);
+        assertEquals(0, CC.CalcPolesBack()); // 270-30=240 : 0
+
+        CC = new CarportCalculator(210, 310, 310);
+        assertEquals(0, CC.CalcPolesBack()); // 310-30=280 : 0
+
+        CC = new CarportCalculator(210, 310, 330);
+        assertEquals(0, CC.CalcPolesBack()); // 330-30=300 : 0
+
+        CC = new CarportCalculator(210, 310, 720);
+        assertEquals(2, CC.CalcPolesBack()); // 720-30=690 : 2
+
+        CC = new CarportCalculator(210, 310, 10000);
+        assertEquals(33, CC.CalcPolesBack()); // 10000-30=9970 : 33
+    }
+
+    @Test
+    public void CalcCarportRoofWidth() { 
+        CarportCalculator CC = new CarportCalculator(210, 310, 270);
+        CC.CalcRoofRafterWidth();
+        assertEquals(1, CC.getProductList().size());
+        CC.CalcRoofRafterLength();
+        assertEquals(3, CC.getProductList().size());
+    }
+}
