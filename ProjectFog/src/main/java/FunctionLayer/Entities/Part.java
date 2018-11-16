35bda769bc7f59002d0ec0784f3096cf4d0e5947
Michael <mich561d@hotmail.com>
2018-11-16 11:27:22 +0100

    Changed SQL file error, Working on entities

---
+++ ProjectFog/src/main/java/FunctionLayer/Entities/Part.java
@@ -0,0 +1,13 @@
+package FunctionLayer.Entities;
+
+/**
+ *
+ * @author Michael
+ */
+public class Part {
+    
+    int id;
+    String type, material, size, description, brand;
+    double price;
+    
+}
