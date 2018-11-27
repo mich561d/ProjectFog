15a38492a3dce89ecdf25f9301f9819ff00d29cf
Christian <rygerlunger@gmail.com>
2018-11-27 10:08:45 +0100

    Updated ShedCalculator

--- ProjectFog/src/main/java/FunctionLayer/Calculation/CarportShedCalculator.java
+++ ProjectFog/src/main/java/FunctionLayer/Calculation/CarportShedCalculator.java
@@ -2,8 +2,10 @@ package FunctionLayer.Calculation;
 
 /**
  *
- * @author Michael
+ * @author Michael & Ryge
  */
 public class CarportShedCalculator {
-    
+    //Et skur har 4 hjørner, de to hjørner er allerede sat fra carporten. Vi skal have mindst én ekstra stolpe til midten af forsiden.
+    //Et skur har en beklædning af bræder, 15mm overlap pr. brædt. så to til en pr. brædt. 
+    //Et skur har en dør. 
 }
