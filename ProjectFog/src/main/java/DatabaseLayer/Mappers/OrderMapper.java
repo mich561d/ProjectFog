b87b3cf4d92a19de7cc50911e6b9e805ebfa2791
Michael <mich561d@hotmail.com>
2018-12-09 16:35:51 +0100

    Updated CustomerOrdersCommand via...

--- ProjectFog/src/main/java/DatabaseLayer/Mappers/OrderMapper.java
+++ ProjectFog/src/main/java/DatabaseLayer/Mappers/OrderMapper.java
@@ -1,7 +1,15 @@
 package DatabaseLayer.Mappers;
 
+import DatabaseLayer.DatabaseConnector;
 import FunctionLayer.Entities.Order;
+import FunctionLayer.Enums.OrderStatus;
+import FunctionLayer.Exceptions.FogException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 /**
  *
@@ -9,8 +17,27 @@ import java.util.ArrayList;
  */
 public class OrderMapper {
 
-    public static ArrayList<Order> getAllOrdersByCustomerID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static ArrayList<Order> getAllOrdersByCustomerID(int customerID) throws FogException {
+        try {
+            Connection con = DatabaseConnector.connection();
+            String SQL = "SELECT * FROM order WHERE customerID = ?";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setInt(1, customerID);
+            ResultSet rs = ps.executeQuery();
+            ArrayList<Order> orders = new ArrayList();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String status = rs.getString("orderStatus");
+                String boughtDate = rs.getString("boughtDate");
+                String sentDate = rs.getString("delieveredDate");
+                int productID = rs.getInt("productID");
+                Order order = new Order(id, OrderStatus.valueOf(status), boughtDate, sentDate, productID, customerID);
+                orders.add(order);
+            }
+            return orders;
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new FogException(ex.getMessage(), Level.SEVERE);
+        }
     }
 
 }
--- ProjectFog/src/main/java/DatabaseLayer/Mappers/ProductMapper.java
+++ ProjectFog/src/main/java/DatabaseLayer/Mappers/ProductMapper.java
@@ -1,6 +1,13 @@
 package DatabaseLayer.Mappers;
 
+import DatabaseLayer.DatabaseConnector;
 import FunctionLayer.Entities.Carport;
+import FunctionLayer.Exceptions.FogException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
 
 /**
  *
@@ -8,8 +15,32 @@ import FunctionLayer.Entities.Carport;
  */
 public class ProductMapper {
 
-    public static Carport getProductFromOrderByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static Carport getProductFromOrderByID(int id) throws FogException {
+        try {
+            Connection con = DatabaseConnector.connection();
+            String SQL = "SELECT * FROM carport WHERE id = ?";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int carportLength = rs.getInt("length");
+                int carportWidth = rs.getInt("width");
+                int carportHeight = rs.getInt("height");
+                boolean angledRoof = 1 == rs.getByte("roof");
+                int angle = rs.getInt("angle");
+                String roofing = rs.getString("roofing");
+                boolean hasShed = 1 == rs.getByte("shed");
+                int shedLength = rs.getInt("shedLength");
+                int shedWidth = rs.getInt("shedWidth");
+                String flooring = rs.getString("flooring");
+                Carport product = new Carport(id, carportLength, carportWidth, carportHeight, angledRoof, angle, roofing, hasShed, shedLength, shedWidth, flooring);
+                return product;
+            } else {
+                throw new FogException("Kunne ikke finde det ønskede product!", Level.SEVERE);
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new FogException(ex.getMessage(), Level.SEVERE);
+        }
     }
 
 }
